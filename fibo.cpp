d02bd474364760e6826af54ec7e113abeb9283fb
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2023-11-29 22:11:16 +0530

    Create fibo.cpp

---
+++ fibo.cpp
@@ -0,0 +1,19 @@
+//fibonacci series
+
+recurence relation :
+f(n)=f(n-1)+f(n-2)
+
+
+int fib(int n){
+//base case
+
+if (n==0)
+return 0;
+if (n==1)
+return 1;
+
+int ans= fib(n-1)+fib(n-2);
+
+return ans;
+
+}

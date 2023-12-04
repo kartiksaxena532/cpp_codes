f6fc389f3b506a2f0247ea3b12f764c136546b61
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2023-12-04 18:15:18 +0530

    Create switchcase.cpp

---
+++ switchcase.cpp
@@ -0,0 +1,20 @@
+SWITCH CASE 
+
+
+switch (expression)  {
+    case constant1:
+        // code to be executed if 
+        // expression is equal to constant1;
+        break;
+
+    case constant2:
+        // code to be executed if
+        // expression is equal to constant2;
+        break;
+        .
+        .
+        .
+    default:
+        // code to be executed if
+        // expression doesn't match any constant
+}

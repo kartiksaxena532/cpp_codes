073a9de2802d2fd0a5f87659364849f0c1cec909
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2023-12-06 18:23:57 +0530

    Create oneThirdARRRAY.cpp

---
+++ oneThirdARRRAY.cpp
@@ -0,0 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int n;
+void printArray(int arr[],int size){
+    cout<<"printing the array"<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<"";
+        
+    }
+}
+//ek ek baar karne ki bajajye humne ek loop ka function hi bana diya
+//usko baar baar call kar liya
+int main() {
+        int n = 15;
+    //printArray(third, 15);
+    int thirdSize = sizeof(third)/sizeof(int);
+    cout<<" Size of Third is " << thirdSize <<endl;
+    return 0;
+}

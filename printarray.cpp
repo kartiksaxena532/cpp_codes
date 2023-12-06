e7a45eb9f00d1ddf2bb49f0085b5278d16b0318e
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2023-12-06 18:23:30 +0530

    Create printarray.cpp

---
+++ printarray.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5]={5,67,78,67,6};
+    cout<<arr[2];
+    int arr2[15]={2,7};
+    int n =15;
+    cout<<"printing the array"<<endl;
+    
+    for(int i=0;i<n;i++){
+        cout<< arr2[i]<<" ";
+        
+    }
+    return 0;
+}
+
+27000000000

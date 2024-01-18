b7ef494436112e199a48be250259378bd4eb3ae4
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-01-18 16:48:30 +0530

    Create queue.cpp

---
+++ queue.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+  queue<int>q;
+  q.push(1);
+   q.push(2);
+    q.push(3);
+   q.push(4);
+   
+cout<<"front element is "<<q.front()<<endl;
+
+cout<<"rear element is "<<q.size()<<endl;
+
+
+
+}

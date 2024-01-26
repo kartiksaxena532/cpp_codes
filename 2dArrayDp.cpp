9114ca6ff07b7a48abb3123e07c14487f2bdf226
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-01-26 21:02:53 +0530

    Create 2dArrayDp.cpp

---
+++ 2dArrayDp.cpp
@@ -0,0 +1,8 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+// agar banani ho koi bhi 2d array 
+
+vector<vector<int>>dp(m,vector<int>dp(n,0)) //generally used for matrix

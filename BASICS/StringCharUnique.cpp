e5d8166175ec8054dc742950084bfff0debd28d2
Kartik Saxena <karrtiksaxena@gmail.com>
2024-02-05 23:39:07 +0530

    new commnets

--- BASICS/StringCharUnique.cpp
+++ BASICS/StringCharUnique.cpp
@@ -10,3 +10,8 @@ public:
     }
 }; 
 
+// range of for loop 
+// from a to s auto will make a range of for loop
+//using undordered map in of the range isme hum map mein string ko store karenge 
+// fir ek doosra loop chalayenge jisme hum check karenge ki kis index pe string ki value 1 hai toh hum us index ko return karenge
+//using undordered map in of the range isme hum map mein string ko store karenge

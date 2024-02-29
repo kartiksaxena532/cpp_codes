b1b93a3abaadb7542ec1b1dec45d79a717edb5be
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-03-01 00:23:57 +0530

    Update NewFucntion.java

--- NewFucntion.java
+++ NewFucntion.java
@@ -3,10 +3,10 @@
 public class Main
 {
 	public static void main(String[] args) {
-	helloKartik();
+	helloKartik(" kartik");
 	}
 	
-	public static void helloKartik(){
-	    	System.out.println("Hello World");
+	public static void helloKartik(String name){
+	    	System.out.println("Hello" + name);
 	}
 }

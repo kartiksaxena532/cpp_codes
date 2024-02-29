c02bf67bd41defd81a8a3eb55944f2ef3e51bb41
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-03-01 00:34:15 +0530

    Update NewFucntion.java

--- NewFucntion.java
+++ NewFucntion.java
@@ -7,10 +7,21 @@ public class Main
 	public static void main(String[] args) {
 
 	helloKartik("kartik",56);
+	int ohmax = max(4,18);
+	System.out.println(ohmax);
 	}
 	
 	public static void helloKartik(String name , int age){
 	 
 	    	System.out.println("Hello " + name + " your age is " + age);
+	}
+	
+	public static int max( int num1 , int num2){
+	    if ( num1 > num2){
+	        return num1;
+	    }
+	    else{
+	        return num2;
+	    }
 	}
 }

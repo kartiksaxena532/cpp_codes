aa511ce24c130714222fdd032e7a65fd6368d402
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-02-29 23:23:00 +0530

    Create Practice.java

---
+++ Practice.java
@@ -0,0 +1,26 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int name =  Integer.MAX_VALUE ;
+		System.out.println("Hello World " + name);
+		
+		int myNumber = 1; 
+		myNumber +=1;
+		System.out.println(myNumber);
+			myNumber -=1;
+			System.out.println(myNumber);
+				myNumber *=3;
+				System.out.println(myNumber);
+					myNumber /=2;
+					System.out.println(myNumber);
+		float num1 = 8f;
+		float num2 =6;
+		
+		System.out.println(num1+num2);
+		System.out.println(num1-num2);
+		System.out.println(num1*num2);
+		System.out.println(num1%num2);
+		
+		
+	}
+}

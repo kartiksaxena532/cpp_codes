6edd887a4e6c676889823a65d39cd0239813abc8
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-03-01 00:30:12 +0530

    Update NewFucntion.java

--- NewFucntion.java
+++ NewFucntion.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class Main
 {
-    public static Scanner kartik =  new scanner(System.in);
+    public static Scanner kartik =  new Scanner(System.in);
 	public static void main(String[] args) {
 
-	helloKartik();
+	helloKartik("kartik",56);
 	}
 	
-	public static void helloKartik(String name){
-	    	System.out.println("Hello" + name);
+	public static void helloKartik(String name , int age){
+	 
+	    	System.out.println("Hello " + name + " your age is " + age);
 	}
 }

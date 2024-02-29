214d0156f76c392a10a64d17141711a025286e8a
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-03-01 00:41:53 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -6,7 +6,8 @@ public class Arrays{
 
    public static void comeToMamaHoney (){
        int num[] = {100,200,120,500,699,675};
-       
-       System.out.println(num[1]);
+       for ( int i = 0 ; i<num.length ; i++){
+       System.out.println(num[i]);
+       }
   }
 }

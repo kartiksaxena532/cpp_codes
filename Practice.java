d7af2efbf174d6349d424f646add44066401e02b
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-02-29 23:26:34 +0530

    Update Practice.java

--- Practice.java
+++ Practice.java
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
 public class Main
 {
 	public static void main(String[] args) {
@@ -21,6 +29,11 @@ public class Main
 		System.out.println(num1*num2);
 		System.out.println(num1%num2);
 		
+		
+			System.out.println(myNumber++);
+				System.out.println(myNumber--);
+					System.out.println(--myNumber);
+						System.out.println(++myNumber);
 		
 	}
 }

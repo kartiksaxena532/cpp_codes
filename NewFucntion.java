24e6b7492578e6f3fb14f07cda639897545582ca
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-03-01 00:27:11 +0530

    Update NewFucntion.java

--- NewFucntion.java
+++ NewFucntion.java
@@ -1,9 +1,12 @@
 
+import java.util.Scanner;
 
 public class Main
 {
+    public static Scanner kartik =  new scanner(System.in);
 	public static void main(String[] args) {
-	helloKartik(" kartik");
+
+	helloKartik();
 	}
 	
 	public static void helloKartik(String name){

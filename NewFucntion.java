8f942c811177c50881cd22b3f61f3cb9fe9a2647
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2024-03-01 00:22:43 +0530

    Update NewFucntion.java

--- NewFucntion.java
+++ NewFucntion.java
@@ -1,6 +1,12 @@
-public static void main(Strings[} args){
-  helloKartik();
 
-  
+
+public class Main
+{
+	public static void main(String[] args) {
+	helloKartik();
+	}
+	
+	public static void helloKartik(){
+	    	System.out.println("Hello World");
+	}
 }
-
